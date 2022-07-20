17fab59f098131894a7350b36b00d208ef0ea41b
Brendo Melo <88464238+BrBrendo@users.noreply.github.com>
2022-07-20 00:40:59 -0400

    Add files via upload

---
+++ analisador/src/main/java/com/compilador/analisador/AnalisadorApplication.java
@@ -0,0 +1,10 @@
+package com.compilador.analisador;
+
+
+public class AnalisadorApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+
+}
---
+++ analisador/src/main/java/com/compilador/analisador/Token.java
@@ -0,0 +1,18 @@
+package com.compilador.analisador;
+
+public class Token {
+private int tipo;
+private String termo;
+public int getTipo() {
+	return tipo;
+}
+public void setTipo(int tipo) {
+	this.tipo = tipo;
+}
+public String getTermo() {
+	return termo;
+}
+public void setTermo(String termo) {
+	this.termo = termo;
+}
+}
